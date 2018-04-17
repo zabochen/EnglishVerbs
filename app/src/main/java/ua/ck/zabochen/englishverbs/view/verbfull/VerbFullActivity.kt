a005080c8cf9da033177fdf98cb009b33ae2a412
zabochen <zabochen@gmail.com>
2018-04-17 16:32:17 +0300

    Edit ActivityVerbFull layout

--- app/src/main/java/ua/ck/zabochen/englishverbs/view/verbfull/VerbFullActivity.kt
+++ app/src/main/java/ua/ck/zabochen/englishverbs/view/verbfull/VerbFullActivity.kt
@@ -3,10 +3,7 @@ package ua.ck.zabochen.englishverbs.view.verbfull
 
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
-import android.widget.ImageView
-import android.widget.TextView
 import com.arellomobile.mvp.presenter.InjectPresenter
-import kotlinx.android.synthetic.main.activity_verb_full.*
 import org.jetbrains.anko.AnkoLogger
 import ua.ck.zabochen.englishverbs.R
 import ua.ck.zabochen.englishverbs.model.realm.Verb
@@ -43,46 +40,7 @@ class VerbFullActivity : BaseActivity(),
 
             // Toolbar
             setSupportActionBar(mToolbar)
-
-            // Verb Translation
-            val mVerbTranslation: TextView = activityVerbFull_textView_verbTranslation
-            mVerbTranslation.text = verb.verbTranslation
-
-            // Verb Infinitive
-            val mVerbInfinitive: TextView = activityVerbFull_textView_verb
-            mVerbInfinitive.text = verb.verbInfinitive
-
-            val mVerbInfinitiveTranscription: TextView = activityVerbFull_textView_verbTranscription
-            mVerbInfinitiveTranscription.text = verb.verbInfinitiveTranscription
-
-            val mVerbInfinitiveButtonPlay: ImageView = activityVerbFull_imageView_verbPlay
-            mVerbInfinitiveButtonPlay.setOnClickListener {
-                mVerbFullPresenter.speakVerb(verb.verbInfinitive)
-            }
-
-            // Verb Past Tense
-            val mVerbPastTense: TextView = activityVerbFull_textView_verbPastTense
-            mVerbPastTense.text = verb.verbPastTense
-
-            val mVerbPastTenseTranscription: TextView = activityVerbFull_textView_verbPastTenseTranscription
-            mVerbPastTenseTranscription.text = verb.verbPastTenseTranscription
-
-            val mVerbPastTenseButtonPlay: ImageView = activityVerbFull_imageView_verbPastTensePlay
-            mVerbPastTenseButtonPlay.setOnClickListener {
-                mVerbFullPresenter.speakVerb(verb.verbPastTense)
-            }
-
-            // Verb Past Participle
-            val mVerbPastParticiple: TextView = activityVerbFull_textView_verbPastParticiple
-            mVerbPastParticiple.text = verb.verbPastParticiple
-
-            val mVerbPastParticipleTranscription: TextView = activityVerbFull_textView_verbPastParticipleTranscription
-            mVerbPastParticipleTranscription.text = verb.verbPastParticipleTranscription
-
-            val mVerbPastParticipleButtonPlay: ImageView = activityVerbFull_imageView_verbPastParticiplePlay
-            mVerbPastParticipleButtonPlay.setOnClickListener {
-                mVerbFullPresenter.speakVerb(verb.verbPastParticiple)
-            }
+            supportActionBar?.setDisplayShowTitleEnabled(false)
 
         }
     }
